763403719d34198b93a915c66dff2fdcad648ca2
monalloyd <monapluslloyd@gmail.com>
2023-09-03 20:01:19 +0200

    Update React router

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,26 +1,23 @@
 import React from "react";
-import { Route, createBrowserRouter, createRoutesFromElements, RouterProvider } from 'react-router-dom';
+import { 
+  Route, 
+  createBrowserRouter, 
+  createRoutesFromElements, 
+  RouterProvider 
+} from 'react-router-dom';
 
 import "./App.css";
 
 import HomePage from "./pages/HomePage";
-// import JournalEntryCreator from "./pages/JournalEntryCreator";
-// import JournalEntryUpdater from "./pages/JournalEntryUpdater";
-// import ErrorPage from "./pages/ErrorPage";
+import ErrorPage from "./pages/ErrorPage";
 
-import Header from "./components/Header";
-
-
-//TODO: additional routes:
-// <Route path="/search/:tag" element={<HomePage />} />
-// <Route path="/create" element={<JournalEntryCreator />} />
-// <Route path="/update" element={<JournalEntryUpdater />} />
-// <Route path="*" element={<ErrorPage />} />
+import Sidebar from "./layouts/Sidebar";
 
 const router = createBrowserRouter(
   createRoutesFromElements(
-    <Route path="/" element={<Header />}>
+    <Route path="/" element={<Sidebar />} >
       <Route index element={<HomePage />} />
+      <Route path="*" element={<ErrorPage />} />
     </Route>
   )
 )
