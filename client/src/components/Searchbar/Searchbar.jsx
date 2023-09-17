d3eb0449c7cf2c6b3a3d2c3cb2fa6a93978b60a4
monalloyd <monapluslloyd@gmail.com>
2023-09-17 19:16:11 +0200

    Add a front-end tag filter with dummy tags

--- client/src/components/Searchbar/Searchbar.jsx
+++ client/src/components/Searchbar/Searchbar.jsx
@@ -1,7 +1,55 @@
+import { useNavigate, useLocation } from 'react-router-dom';
+import { useState, useEffect } from "react";
 import "./SearchBar.css";
 
 const SearchBar = () => {
-    return <input type="text" placeholder="Filter tags..." className="tag-filter"></input>
+    const [selectedOption, setSelectedOption] = useState("");
+    const [tags, setTags] = useState(["tag", "tag2", "tag3"]);
+    const navigate = useNavigate();
+    const location = useLocation();
+    const queryParams = new URLSearchParams(location.search);
+    const [selectedTags, setSelectedTags] = useState(
+        queryParams.getAll("tag") || []
+    );
+
+    useEffect(() => {
+        const currentTags = queryParams.getAll("tag") || [];
+        setSelectedTags(currentTags);
+    }, [location.search]);
+
+    const handleTagChange = (event) => {
+        const newTag = event.target.value;
+        setSelectedTags((prevTags) => [...prevTags, newTag]);
+
+        if (!selectedTags.includes(newTag)) {
+            setSelectedTags((prevTags) => [...prevTags, newTag]);
+            queryParams.append("tag", newTag);
+            navigate(`?${queryParams.toString()}`);
+        }
+
+        changeSelectedOption();
+    };
+
+    const changeSelectedOption = () => {
+        setSelectedOption("");
+    }
+
+    return (
+        <div className="select-wrapper">
+        <select 
+            className="tag-filter" 
+            onChange={handleTagChange}
+            value={selectedOption}
+        >
+            <option value="">Filter tags...</option>
+            {
+                tags && tags.map((tag, i) => (
+                    <option key={i} value={tag}>{tag}</option>
+                ))
+            }
+        </select>
+        </div>
+    );
 }
 
 export default SearchBar;
--- client/src/components/Table/Table.jsx
+++ client/src/components/Table/Table.jsx
@@ -1,142 +1,125 @@
-import { Link } from "react-router-dom";
+import { useState, useEffect } from "react";
+import { Link, useLocation, useNavigate } from "react-router-dom";
 import Button from "./../Button";
 import "./Table.css";
 
+const dummyEntries = [
+    {
+        title: "one",
+        date: "date1",
+        tags: ["tag"]
+    },
+    {
+        title: "two",
+        date: "date2",
+        tags: ["tag2"]
+    },
+    {
+        title: "three",
+        date: "date3",
+        tags: ["tag3"]
+    },
+    {
+        title: "four",
+        date: "date4",
+        tags: ["tag", "tag2"]
+    },
+    {
+        title: "five",
+        date: "date5",
+        tags: ["tag", "tag3"]
+    },
+    {
+        title: "six",
+        date: "date6",
+        tags: ["tag2", "tag3"]
+    }
+]
+
 const Table = () => {
-    return <table>
-        <tbody>
-            <tr>
-                <td className="col-1">
-                    <Link to={`/entry/id`}>
-                        this is a looooooooong title
-                    </Link>
-                </td>
-                <td className="col-2">date</td>
-                <td className="col-3">
-                    <span>#tag</span>
-                    <span>#tag2</span>
-                    <span>#tag3</span>
-                </td>
-                <td className="col-4">
-                    <Link to={`/update/id`}>
+    const location = useLocation();
+    const navigate = useNavigate();
+    const queryParams = new URLSearchParams(location.search);
+    const tagsFromUrl = queryParams.getAll("tag");
+    const [entries, setEntries] = useState([]);
+
+    useEffect(() => {
+        const filteredEntries = dummyEntries.filter((entry) => {
+            if (tagsFromUrl.length === 0) {
+                return true;
+            }
+            return tagsFromUrl.every((tag) => entry.tags.includes(tag));
+        });
+        setEntries(filteredEntries);
+    }, [tagsFromUrl]);
+
+    const deleteTag = (tagToDelete) => {
+        const updatedTags = [...tagsFromUrl];
+
+        const indexToDelete = updatedTags.indexOf(tagToDelete);
+        
+        if (indexToDelete !== -1) {
+            updatedTags.splice(indexToDelete, 1);
+
+            queryParams.delete("tag");
+            updatedTags.forEach((tag) => {
+                queryParams.append("tag", tag);
+            });
+
+            navigate(`?${queryParams.toString()}`);
+        }
+    }
+
+    return (
+        <>
+        <div className="tag-container">
+        {
+            tagsFromUrl && tagsFromUrl.map((tag, i) => (
+                <Button 
+                    className={"nav-btn-square tag-btn robo"} 
+                    text={tag} 
+                    key={i}
+                    onClick={() => deleteTag(tag)}
+                />
+            ))
+        }
+        </div>
+        <table>
+            <tbody>
+                {entries.map((entry, i) => (
+                    <tr key={i}>
+                    <td className="col-1">
+                        <Link to={`/entry/id`}>
+                            {entry.title}
+                        </Link>
+                    </td>
+                    <td className="col-2">{entry.date}</td>
+                    <td className="col-3">
+                        {
+                            entry.tags && entry.tags.map((tag, i) => (
+                                <span key={"tag-" + i}>{tag}</span>
+                            ))
+                        }
+                    </td>
+                    <td className="col-4">
+                        <Link to={`/update/id`}>
+                            <Button 
+                                className={"nav-btn-square update-btn robo"} 
+                                text={"Update"}
+                            />
+                        </Link>
                         <Button 
-                            className={"nav-btn-square update-btn robo"} 
-                            text={"Update"}
+                            className={"nav-btn-square delete-btn robo"} 
+                            text={"Delete"} 
+                            onClick={(e) => console.log(e.target)}
                         />
-                    </Link>
-                    <Button 
-                        className={"nav-btn-square delete-btn robo"} 
-                        text={"Delete"} 
-                        onClick={(e) => console.log(e.target)}
-                    />
-                </td>
-            </tr>
-            <tr>
-                <td>
-                    <Link to={`/entry/id`}>
-                        title
-                    </Link>
-                </td>
-                <td>date</td>
-                <td>
-                    <span>#tag</span>
-                    <span>#tag2</span>
-                    <span>#tag3</span>
-                </td>
-                <td className="right">
-                    <Link to={`/update/id`}>
-                        <Button 
-                            className={"nav-btn-square update-btn robo"} 
-                            text={"Update"}
-                        />
-                    </Link>
-                    <Button 
-                        className={"nav-btn-square delete-btn robo"} 
-                        text={"Delete"} 
-                        onClick={(e) => console.log(e.target)}
-                    />
-                </td>
-            </tr>
-            <tr>
-                <td>
-                    <Link to={`/entry/id`}>
-                        title
-                    </Link>
-                </td>
-                <td>date</td>
-                <td>
-                    <span>#tag</span>
-                    <span>#tag2</span>
-                    <span>#tag3</span>
-                </td>
-                <td className="right">
-                    <Link to={`/update/id`}>
-                        <Button 
-                            className={"nav-btn-square update-btn robo"} 
-                            text={"Update"}
-                        />
-                    </Link>
-                    <Button 
-                        className={"nav-btn-square delete-btn robo"} 
-                        text={"Delete"} 
-                        onClick={(e) => console.log(e.target)}
-                    />
-                </td>
-            </tr>
-            <tr>
-                <td>
-                    <Link to={`/entry/id`}>
-                        title
-                    </Link>
-                </td>
-                <td>date</td>
-                <td>
-                    <span>#tag</span>
-                    <span>#tag2</span>
-                    <span>#tag3</span>
-                </td>
-                <td className="right">
-                    <Link to={`/update/id`}>
-                        <Button 
-                            className={"nav-btn-square update-btn robo"} 
-                            text={"Update"}
-                        />
-                    </Link>
-                    <Button 
-                        className={"nav-btn-square delete-btn robo"} 
-                        text={"Delete"} 
-                        onClick={(e) => console.log(e.target)}
-                    />
-                </td>
-            </tr>
-            <tr>
-                <td>
-                    <Link to={`/entry/id`}>
-                        title
-                    </Link>
-                </td>
-                <td>date</td>
-                <td>
-                    <span>#tag</span>
-                    <span>#tag2</span>
-                    <span>#tag3</span>
-                </td>
-                <td className="right">
-                    <Link to={`/update/id`}>
-                        <Button 
-                            className={"nav-btn-square update-btn robo"} 
-                            text={"Update"}
-                        />
-                    </Link>
-                    <Button 
-                        className={"nav-btn-square delete-btn robo"} 
-                        text={"Delete"} 
-                        onClick={(e) => console.log(e.target)}
-                    />
-                </td>
-            </tr>
-        </tbody>
-    </table>
+                    </td>
+                </tr>
+                ))}
+            </tbody>
+        </table>
+        </>
+    )
 }
 
 export default Table;
