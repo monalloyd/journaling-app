c130011ed1796a90766e298a33cf2103dc5e4a42
monalloyd <monapluslloyd@gmail.com>
2023-11-10 09:47:54 +0100

    Clean up unused components

--- client/src/components/LoadingSpinner/LoadingSpinner.jsx
+++
@@ -1,9 +0,0 @@
-//TODO: design loading spinner
-
-import "./LoadingSpinner.css";
-
-const LoadingSpinner = () => {
-    return <div>Loading...</div>;
-}
-
-export default LoadingSpinner;
---
+++ client/src/constants/Constants.jsx
@@ -0,0 +1,3 @@
+const quote = "If you're going through hell, keep going - Winston Churchill";
+
+export { quote };
