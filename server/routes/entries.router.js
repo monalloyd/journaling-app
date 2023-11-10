e6e4b107ab349f57b2fe663747aa2d695a95cd69
monalloyd <monapluslloyd@gmail.com>
2023-11-10 09:42:52 +0100

    Write crud operations

--- server/routes/entries.router.js
+++ server/routes/entries.router.js
@@ -8,4 +8,40 @@ entriesRouter.get("/", async (req, res) => {
     return res.json(entries);
 });
 
+entriesRouter.get("/:id", async (req, res) => {
+    const entry = await EntryModel.find({ _id: req.params.id }).populate("tags");
+    return res.json(entry);
+});
+
+entriesRouter.post("/", async (req, res, next) => {
+    try {
+        const saved = await EntryModel.create(req.body);
+        return res.json(saved);
+    } catch (err) {
+        return next(err);
+    }
+});
+
+entriesRouter.put("/:id", async (req, res, next) => {
+  const filter = { _id: req.params.id };
+  let update = { $set: { ...req.body } };
+  const options = { new: true };
+
+  try {
+      const entry = await EntryModel.findOneAndUpdate(filter, update, options);
+      return res.json(entry);
+    } catch (err) {
+      return next(err);
+    }
+});
+
+entriesRouter.delete("/:id", async (req, res, next) => {
+  try {
+    const res = await EntryModel.deleteOne({_id: req.params.id});
+    return res;
+  } catch (err) {
+    return next(err);
+  }
+});
+
 module.exports = entriesRouter;
--- server/routes/tags.router.js
+++ server/routes/tags.router.js
@@ -8,4 +8,18 @@ tagsRouter.get("/", async (req, res) => {
     return res.json(tags);
 });
 
+tagsRouter.get("/:name", async (req, res) => {
+    const tag = await TagModel.find({"name": { $regex: req.params.name, "$options": "i"}});
+    return res.json(tag);
+});
+
+tagsRouter.post("/", async (req, res, next) => {
+    try {
+        const saved = await TagModel.create(req.body);
+        return res.json(saved);
+    } catch (err) {
+        return next(err);
+    }
+});
+
 module.exports = tagsRouter;
